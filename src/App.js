28207435a59c9518dbffd36683626cd9eb94f1d5
daniel <sahomelabs@gmail.com>
2024-07-18 12:43:30 -0400

    incomeform update and appjs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React, { useState } from 'react';
 import Header from './Components/Header/Header';
 import ExpenseForm from './Components/ExpenseForm/ExpenseForm';
+import IncomeForm from './Components/IncomeForm/IncomeForm';
 import ExpenseList from './Components/ExpenseList/ExpenseList';
 import Summary from './Components/Summary/Summary';
 import Footer from './Components/Footer/Footer';
 import './App.css';
 
 const App = () => {
+  const [income, setIncome] = useState(0);
   const [expenses, setExpenses] = useState([]);
 
   const addExpense = (expense) => {
@@ -17,9 +19,10 @@ const App = () => {
   return (
     <div className="App">
       <Header />
+      <IncomeForm setIncome={setIncome} />
       <ExpenseForm addExpense={addExpense} />
       <ExpenseList expenses={expenses} />
-      <Summary expenses={expenses} />
+      <Summary income={income} expenses={expenses} />
       <Footer />
     </div>
   );
