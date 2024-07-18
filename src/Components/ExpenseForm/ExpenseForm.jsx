327e781d42893f0561dd487b1fbece1a27bbe42b
daniel <sahomelabs@gmail.com>
2024-07-18 17:09:38 -0400

    added expense category

--- src/Components/ExpenseForm/ExpenseForm.jsx
+++ src/Components/ExpenseForm/ExpenseForm.jsx
@@ -1,4 +1,4 @@
-// src/components/ExpenseForm.jsx
+// ExpenseForm.jsx
 import React, { useState } from 'react';
 
 const ExpenseForm = ({ addExpense }) => {
@@ -11,22 +11,80 @@ const ExpenseForm = ({ addExpense }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     addExpense(expense);
-    setExpense({ name: '', amount: '', category: '', date: '', dueDate: ''});
+    setExpense({ name: '', amount: '', category: '', date: '', dueDate: '' });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="name" value={expense.name} onChange={handleChange} placeholder="Expense Name" required />
-      <input type="number" name="amount" value={expense.amount} onChange={handleChange} placeholder="Amount" required />
-      <input type="text" name="category" value={expense.category} onChange={handleChange} placeholder="Category" required />
-      <label>
-        Added Date:
-      <input type="date" name="date" value={expense.date} onChange={handleChange} required />
-      </label>
-      <label>
-        Due Date: 
-      <input type='date' name='dueDate' value={expense.dueDate} onChange={handleChange} placeholder='Due Date' required />
-      </label>
+
+
+
+
+      
+      <input
+        type="text"
+        name="name"
+        value={expense.name}
+        onChange={handleChange}
+        placeholder="Expense Name"
+        required
+      />
+      <input
+        type="number"
+        name="amount"
+        value={expense.amount}
+        onChange={handleChange}
+        placeholder="Amount"
+        required
+      />
+      <select
+        name="category"
+        value={expense.category}
+        onChange={handleChange}
+        required
+      >
+        <option value="">Select Category</option>
+        <optgroup label="HOUSING">
+          <option value="Mortgage">Mortgage</option>
+          <option value="Rent">Rent</option>
+          <option value="Phone Number">Phone Number</option>
+          <option value="Electricity">Electricity</option>
+          <option value="Gas">Gas</option>
+          <option value="Water and sewer">Water and sewer</option>
+          <option value="Cable">Cable</option>
+          <option value="Waste removal">Waste removal</option>
+          <option value="Maintenance or repairs">Maintenance or repairs</option>
+          <option value="Supplies">Supplies</option>
+          <option value="Other">Other</option>
+        </optgroup>
+        <optgroup label="ENTERTAINMENT"> 
+            <option value="Netflixt">Netflix</option> 
+            <option value="Hulu">Hulu</option> 
+            <option value="Amazon Prime">Amazon Prime</option> 
+            <option value="Disney Plus">Disney Plus"</option> 
+            <option value="Apple TV Plus">Apple TV Plus"</option> 
+            <option value="Paramount">Paramount</option> 
+            <option value="YouTube Premium">YouTube Premium</option> 
+            <option value="YouTube Tv">YouTube Tv</option> 
+            <option value="Other">Other</option> 
+        </optgroup>
+
+
+      </select>
+      <input
+        type="date"
+        name="date"
+        value={expense.date}
+        onChange={handleChange}
+        required
+      />
+      <input
+        type="date"
+        name="dueDate"
+        value={expense.dueDate}
+        onChange={handleChange}
+        required
+      />
       <button type="submit">Add Expense</button>
     </form>
   );
