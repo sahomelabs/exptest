86cadf0851bb4507875027866a6db284b01b9595
daniel <sahomelabs@gmail.com>
2024-07-19 10:50:15 -0400

    ContactUs page initial added

---
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,13 @@
+// src/pages/ContactUs.jsx
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div className="container">
+      <h2>Contact</h2>
+      <p>Here are the terms of use...</p>
+    </div>
+  );
+};
+
+export default ContactUs;
