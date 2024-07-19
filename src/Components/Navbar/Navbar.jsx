b7808de94e3d35f5eea3e3549a7fc7e74704e1d8
daniel <sahomelabs@gmail.com>
2024-07-19 15:25:30 -0400

    signOut and SignIn add to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 
-const Navbar = () => {
+const Navbar = ({isAuthenticated}) => {
   return (
     <nav className="navbar">
       <ul>
         <li><Link to="/">Home</Link></li>
+        {isAuthenticated ? (
+          <li><Link to="/signout">Sign Out</Link></li>
+        ) : (
+          <li><Link to="/signin">Sign In</Link></li>
+        )}
       </ul>
     </nav>
   );
