b90a4412387a05aff2a66c7e6de9524568efe946
daniel <sahomelabs@gmail.com>
2024-07-19 10:47:50 -0400

    pages folder and termsofuse created

---
+++ src/Pages/TermsofUse/TermsOfUse.jsx
@@ -0,0 +1,13 @@
+// src/pages/TermsOfUse.jsx
+import React from 'react';
+
+const TermsOfUse = () => {
+  return (
+    <div className="container">
+      <h2>Terms of Use</h2>
+      <p>Here are the terms of use...</p>
+    </div>
+  );
+};
+
+export default TermsOfUse;
