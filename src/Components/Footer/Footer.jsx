67b0cdac9104a284aabaf2b7f6c869654348c82c
daniel <sahomelabs@gmail.com>
2024-07-19 10:45:23 -0400

    footer menu css update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-links">
-        <a href="/terms-of-use">Terms of Use</a>
         <a href="/contact-us">Contact Us</a>
+        <a href="/terms-of-use">Terms of Use</a>
         <a href="/privacy-policy">Privacy Policy</a>
       </div>
         <p>&copy; 2024 Expense Tracker</p>
