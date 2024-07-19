792c8b9df2de83e51b5a92f67416c170e64cf21a
daniel <sahomelabs@gmail.com>
2024-07-19 10:54:33 -0400

    Privacy page initial added

---
+++ src/Pages/Privacy/Privacy.jsx
@@ -0,0 +1,13 @@
+// src/pages/privacy.jsx
+import React from 'react';
+
+const Privacy = () => {
+  return (
+    <div className="container">
+      <h2>Privacy</h2>
+      <p>Here are the terms of use...</p>
+    </div>
+  );
+};
+
+export default Privacy;
