e3bf7c8927b272e1089390c342805bd53aa0ab8a
daniel <sahomelabs@gmail.com>
2024-07-19 15:10:54 -0400

    signOut import css remove

--- src/Components/SignOut/SignOut.jsx
+++ src/Components/SignOut/SignOut.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './SignOut.css';
 
 const SignOut = ({ setIsAuthenticated }) => {
   const navigate = useNavigate();
