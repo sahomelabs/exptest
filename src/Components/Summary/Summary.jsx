bcd7008767ce222ea6d477d3d37d22349bce0d6f
daniel <sahomelabs@gmail.com>
2024-07-19 20:34:40 -0400

    update summary css

--- src/Components/Summary/Summary.jsx
+++ src/Components/Summary/Summary.jsx
@@ -1,16 +1,19 @@
 // summary.jsx
-
 import React from 'react';
+import './Summary.css';
 
 const Summary = ({ income, expenses }) => {
   const totalExpenses = expenses.reduce((acc, expense) => acc + parseFloat(expense.amount), 0);
   const remainingBalance = income - totalExpenses;
+  const balanceClass = remainingBalance >= 0 ? 'positive' : 'negative';
+
 
   return (
-    <div>
+    <div className="summary">
       <h2>Total Income: ${income.toFixed(2)}</h2>
       <h2>Total Expenses: ${totalExpenses.toFixed(2)}</h2>
-      <h2>Remaining Balance: ${remainingBalance.toFixed(2)}</h2>
+      <h2 className={balanceClass}>Remaining Balance: ${remainingBalance.toFixed(2)}</h2>
+
     </div>
   );
 };
