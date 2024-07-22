dc05e4776591909d0f8843a5a9f966067ee05548
sahomelabs <sahomelabs@gmail.com>
2024-07-22 18:46:28 -0400

    add expensejs into models folder

---
+++ Backend/Models/Expense.js
@@ -0,0 +1,31 @@
+// backend/models/Expense.js
+const mongoose = require('mongoose');
+
+const ExpenseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    categoryGroup: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    dueDate: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Expense', ExpenseSchema);
