a8246a15ddfb3847847151a8bcd79a3c2691d22b
sahomelabs <sahomelabs@gmail.com>
2024-07-31 17:26:01 -0400

    Signout Notification

--- src/Components/SignOut/SignOut.jsx
+++ src/Components/SignOut/SignOut.jsx
@@ -1,19 +1,27 @@
 // src/Components/SignOut/SignOut.jsx
-import React from 'react';
+import React, { useState }from 'react';
 import { useNavigate } from 'react-router-dom';
+import "./SignOut.css";
 
 const SignOut = ({ setIsAuthenticated }) => {
+  const [showNotification, setShowNotification] = useState(false);
   const navigate = useNavigate();
 
   const handleSignOut = () => {
     localStorage.removeItem('token'); // Remove token from localStorage
     setIsAuthenticated(false); // Update authentication state
+    setShowNotification(true); // Show notification
+    setTimeout(() => {
+      setShowNotification(false);
     navigate('/signin'); // Redirect to sign in page
-  };
+  }, 3000);
+
+};
 
   return (
     <div className="signout">
-      <h2>You are signed out</h2>
+            {showNotification && <div className="notification">You have successfully signed out</div>}
+      {/* <h2>You are about to signed out</h2> */}
       <button onClick={handleSignOut}>Sign Out</button>
     </div>
   );
